4f4a5cb4326bde1ae6561b8b6541f3afc712f509
japago25andes <j.pardog@uniandes.edu.co>
2024-03-21 20:57:43 -0500

    Finalizacion con pipes personalizados

---
+++ src/app/products/pages/order/order.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import { Color, Hero } from '../../interfaces/hero.interface';
+
+@Component({
+  selector: 'products-order',
+  templateUrl: './order.component.html',
+})
+export class OrderComponent{
+
+  public isUpperCase: boolean = false;
+  public orderBy: keyof Hero | undefined | ''= '';
+
+
+  public heroes: Hero[] = [
+    {
+      name: 'Superman',
+      canFly: true,
+      color: Color.blue,
+    },
+    {
+      name: 'Batman',
+      canFly: false,
+      color: Color.black,
+    },
+    {
+      name: 'Robin',
+      canFly: false,
+      color: Color.green,
+    },
+    {
+      name: 'Flash',
+      canFly: false,
+      color: Color.red,
+    },
+    {
+      name: 'Linterna Verde',
+      canFly: true,
+      color: Color.green,
+    },
+  ];
+
+
+
+  toggleUpperCase(): void {
+    this.isUpperCase = !this.isUpperCase;
+  }
+
+  changeOrder(value: keyof Hero | ''): void {
+    this.orderBy = value;
+  }
+
+
+}
--- src/app/shared/components/menu/menu.component.ts
+++ src/app/shared/components/menu/menu.component.ts
@@ -37,8 +37,9 @@ export class MenuComponent implements OnInit {
             icon: 'pi pi-cog',
             items: [
               {
-                label: 'Mayúsculas',
+                label: 'Custom Pipes',
                 icon: 'pi pi-cog',
+                routerLink: 'custom'
               },
             ]
           }
